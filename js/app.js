dc66f972dec212b82a4b4b7750f0ce7dce05503a
Cawley90 <chrisjam90@gmail.com>
2014-03-25 21:37:23 -0700

    v5
    Have cool stance along with music working.

--- js/app.js
+++ js/app.js
@@ -34,11 +34,37 @@ $(document).ready(function() {
   
   });
 
+  $(document).keydown(function(e){
+  	if(e.which == 88) {
+  	playSong();	
+  	$('.ryu-ready').hide();
+   	$('.ryu-cool').show();
+   	
+  		}
+	  
+  	})
+
+  .keyup(function(e){
+  	if(e.which == 88) {
+  		$("#ssj3")[0].pause();
+		$('.ryu-cool').hide();
+  		$('.ryu-ready').show();
+
+  	}
+  	
+  	});
+
 });
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.6;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
+
+}
+
+function playSong () {
+	$('#ssj3')[0].volume = 0.5;
+	$('#ssj3')[0].play();
 
 }
